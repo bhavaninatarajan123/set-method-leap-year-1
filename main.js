c55dfad80e5aa172c60cd9e139a29f02a9404f1a
priyankacute <bhavanimurugesh2000@gmail.com>
2023-08-02 11:48:47 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,26 @@
+var today=new Date();
+var year=today.getFullYear(today.setFullYear("2024"));
+document.write("Given year="+year+"<br><br>");
+if(year%4==0){
+    document.write(" the given year is leap year");
+}
+else{
+    document.write(" the given year is not a leap year");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
